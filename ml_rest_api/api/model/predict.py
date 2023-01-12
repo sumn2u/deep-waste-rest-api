5471c60523783afebc4f8a6320ca50f89c2187fc
suman <suman.kunwar@daliaresearch.com>
2023-01-12 07:59:34 -0600

    Add Heroku deployment files

--- ml_rest_api/api/model/predict.py
+++ ml_rest_api/api/model/predict.py
@@ -3,44 +3,20 @@ import json
 from typing import Type, Dict
 from aniso8601 import parse_date, parse_datetime
 from flask import request
-from flask_restx import Resource, Model, fields
+from flask_restx import Resource, Model, fields, reqparse
 from ml_rest_api.api.restx import api, FlaskApiReturnType, MLRestAPINotReadyException
 from ml_rest_api.ml_trained_model.wrapper import trained_model_wrapper
+from ml_rest_api.ml_trained_model.ml_trained_model import full_path
+from werkzeug.datastructures import FileStorage
 
-
-def build_api_model() -> Model:
-    """
-    Returns a Flask-RESTX Api Model based on the sample dict returned by the trained model wrapper.
-    This will be used to validate input and automatically generate the Swagger prototype.
-    """
-    fields_classes_map: Dict = {
-        "str": fields.String,
-        "int": fields.Integer,
-        "float": fields.Float,
-        "bool": fields.Boolean,
-        "datetime": fields.DateTime,
-        "date": fields.Date,
-    }
-    model_dict: Dict = {}
-    model_sample: Dict = trained_model_wrapper.sample()
-    if model_sample:
-        for key, value in model_sample.items():
-            fields_class: Type[fields.Raw] = fields_classes_map.get(
-                type(value).__name__, fields.String
-            )
-            if type(value).__name__ == "str":
-                try:
-                    parse_date(value)
-                    fields_class = fields.Date
-                except ValueError:
-                    pass
-                try:
-                    parse_datetime(value)
-                    fields_class = fields.DateTime
-                except ValueError:
-                    pass
-            model_dict[key] = fields_class(example=value, readonly=True, required=True)
-    return api.model("input_vector", model_dict)
+"""
+This will be used to validate input and automatically generate the Swagger prototype.
+"""
+upload_parser = reqparse.RequestParser()
+upload_parser.add_argument('file', location='files',
+                           type=FileStorage, required=True)
+upload_parser.add_argument('classifiers',
+                           required=True, action='append', help="['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']")
 
 
 ns = api.namespace(  # pylint: disable=invalid-name
@@ -54,8 +30,8 @@ ns = api.namespace(  # pylint: disable=invalid-name
 class ModelPredict(Resource):
     """Implements the /model/predict POST method."""
 
-    @staticmethod
-    @api.expect(build_api_model())
+    # @staticmethod
+    @api.expect(upload_parser)
     @api.doc(
         responses={
             200: "Success",
@@ -64,12 +40,30 @@ class ModelPredict(Resource):
             503: "Server Not Ready",
         }
     )
-    def post() -> FlaskApiReturnType:
+    def post(self):
         """
         Returns a prediction using the model.
         """
-        if not trained_model_wrapper.ready():
-            raise MLRestAPINotReadyException()
-        data = dict(json.loads(request.data))
-        ret = trained_model_wrapper.run(data)
+        # if not trained_model_wrapper.ready():
+        #     raise MLRestAPINotReadyException()
+        if 'file' not in request.files:
+            return { 'error': 'No file part in request' }, 400
+
+        if request.files['file'].filename == '':
+            return { "message": 'No selected file'}, 400
+
+        temp_file = full_path("temp/upload.jpg")
+        file = request.files['file']
+        if file.filename == '':
+            return { 'error': 'No selected file' }, 400
+        img_bytes = file.read()
+        with open(temp_file, mode="wb") as jpg:
+            jpg.write(img_bytes)
+        
+        args = upload_parser.parse_args()
+        model_dict: Dict = {
+            "image": temp_file,
+            "classifiers": args['classifiers']
+        }
+        ret = trained_model_wrapper.run(model_dict)
         return ret, 200
